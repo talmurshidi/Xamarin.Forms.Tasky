67464fb0acb3b61208500d05232b2c323823acfd
Tamer Almurshidi <tamer_tamer_1000@hotmail.com>
2018-04-05 00:19:55 +0200

    Update xamarin forms to 2.5.1

---
+++ Tasky/Tasky.Android/RendererAndroid/ButtonAndroid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Tasky.Controlls;
+using Tasky.Droid.RendererAndroid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer( typeof( ButtonControl ), typeof( ButtonAndroid ) )]
+namespace Tasky.Droid.RendererAndroid
+{
+    public class ButtonAndroid : ButtonRenderer
+    {
+        public ButtonAndroid( Context context ) : base( context )
+        {
+
+        }
+    }
+}
---
+++ Tasky/Tasky/Controlls/ListViewControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Tasky.Controlls
+{
+    public class ListViewControl : ListView
+    {
+        public ListViewControl() : base( ListViewCachingStrategy.RecycleElement )
+        {
+
+        }
+    }
+}
